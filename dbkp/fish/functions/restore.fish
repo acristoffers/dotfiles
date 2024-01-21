695c176fb8fbe06b4f9df399ee90881968d8440f
Álan Crístoffer <acristoffers@startmail.com>
2024-01-21 17:22:24 +0100

    dom 21 jan 2024 17:22:24 CET

--- dbkp/fish/functions/restore.fish
+++ dbkp/fish/functions/restore.fish
@@ -1,27 +1,25 @@
 function restore
-    block -l
+  block -l
 
-    if not test -d ~/Developer/GitHub/dotfiles
-        mkdir ~/Developer/GitHub
-        git clone --recurse-submodule https://github.com/acristoffers/dotfiles ~/Developer/GitHub/dotfiles
-    else
-        pushd ~/Developer/GitHub/dotfiles
-        git pull
-        git submodule update --remote --merge
-        popd
-    end
+  if not test -d ~/Developer/GitHub/dotfiles
+    mkdir ~/Developer/GitHub
+    git clone --recurse-submodule https://github.com/acristoffers/dotfiles ~/Developer/GitHub/dotfiles
+  else
+    pushd ~/Developer/GitHub/dotfiles
+    git pull
+    popd
+  end
 
-    dbkp restore ~/Developer/GitHub/dotfiles/dbkp.json
+  dbkp restore ~/Developer/GitHub/dotfiles/dbkp.toml
 
-    if not test -d ~/Developer/GitHub/dotfiles-secrets
-        mkdir ~/Developer/GitHub
-        git clone --recurse-submodule https://github.com/acristoffers/dotfiles-secrets ~/Developer/GitHub/dotfiles-secrets
-    else
-        pushd ~/Developer/GitHub/dotfiles-secrets
-        git pull
-        git submodule update --remote --merge
-        popd
-    end
+  if not test -d ~/Developer/GitHub/dotfiles-secrets
+    mkdir ~/Developer/GitHub
+    git clone --recurse-submodule https://github.com/acristoffers/dotfiles-secrets ~/Developer/GitHub/dotfiles-secrets
+  else
+    pushd ~/Developer/GitHub/dotfiles-secrets
+    git pull
+    popd
+  end
 
-    dbkp restore ~/Developer/GitHub/dotfiles-secrets/dbkp.json
+  dbkp restore ~/Developer/GitHub/dotfiles-secrets/dbkp.toml
 end
