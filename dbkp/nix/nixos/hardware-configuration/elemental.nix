388063ab0db5e116fef124fc657f22f216865e63
Álan Crístoffer <acristoffers@startmail.com>
2024-02-19 15:18:16 +0100

    Mon Feb 19 15:18:16 2024

---
+++ dbkp/nix/nixos/hardware-configuration/elemental.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules = [ "vmd" "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/4401011e-b546-4582-b6aa-05476825f670";
+    fsType = "ext4";
+  };
+
+  boot.initrd.luks.devices."luks-bc6920fb-944d-48e5-8f73-3f57179ca527".device = "/dev/disk/by-uuid/bc6920fb-944d-48e5-8f73-3f57179ca527";
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/E383-104D";
+    fsType = "vfat";
+    options = [ "umask=077" ];
+  };
+
+  swapDevices = [{ device = "/dev/disk/by-uuid/eb73c555-7f45-49c1-bcd9-805e4e43bc08"; }];
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+}
--- dbkp/nix/nixos/machines/elemental.nix
+++ dbkp/nix/nixos/machines/elemental.nix
@@ -11,14 +11,8 @@ rec {
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
 
+  boot.initrd.luks.devices."luks-3a81fadd-3844-45c6-80fc-a7f0fcf537c8".device = "/dev/disk/by-uuid/3a81fadd-3844-45c6-80fc-a7f0fcf537c8";
   boot.initrd.kernelModules = [ "i915" ];
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-7b41b757-fdb9-44cd-a0cf-f0da2923f49c".device = "/dev/disk/by-uuid/7b41b757-fdb9-44cd-a0cf-f0da2923f49c";
-  boot.initrd.luks.devices."luks-7b41b757-fdb9-44cd-a0cf-f0da2923f49c".keyFile = "/crypto_keyfile.bin";
 
   environment.variables = {
     VDPAU_DRIVER = "va_gl";
@@ -33,6 +27,7 @@ rec {
     libvdpau-va-gl
   ];
 
+  security.lockKernelModules = false;
   security.unprivilegedUsernsClone = true;
 
   services.fstrim.enable = true;
