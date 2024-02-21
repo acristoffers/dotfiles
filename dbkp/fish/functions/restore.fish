f9085b2e6fc3b841325e362722a2a57c2ab96b09
Álan Crístoffer <acristoffers@startmail.com>
2024-02-21 19:32:09 +0100

    Wed Feb 21 19:32:09 2024

--- dbkp/fish/functions/restore.fish
+++ dbkp/fish/functions/restore.fish
@@ -9,29 +9,31 @@ function restore
   else
     pushd ~/Developer/GitHub/dotfiles
     git pull
+    or begin
+      echo Error updating with git in dotfiles
+      exit 1
+    end
     popd
   end
 
   dbkp restore ~/Developer/GitHub/dotfiles/dbkp.toml
 
-  if test -z "$_flag_public"
-    if not test -d ~/Developer/GitHub/dotfiles-secrets
-      mkdir ~/Developer/GitHub
-      git clone --recurse-submodule https://github.com/acristoffers/dotfiles-secrets ~/Developer/GitHub/dotfiles-secrets
-      or begin
-        echo Error updating with git
-        exit 1
-      end
-    else
-      pushd ~/Developer/GitHub/dotfiles-secrets
-      git pull
-      or begin
-        echo Error updating with git
-        exit 1
-      end
-      popd
-    end
-
-    dbkp restore ~/Developer/GitHub/dotfiles-secrets/dbkp.toml
+  if test -n "$_flag_public"
+    exit 0
   end
+
+  if not test -d ~/Developer/GitHub/dotfiles-secrets
+    mkdir ~/Developer/GitHub
+    git clone --recurse-submodule https://github.com/acristoffers/dotfiles-secrets ~/Developer/GitHub/dotfiles-secrets
+  else
+    pushd ~/Developer/GitHub/dotfiles-secrets
+    git pull
+    or begin
+      echo Error updating with git in dotfiles-secrets
+      exit 1
+    end
+    popd
+  end
+
+  dbkp restore ~/Developer/GitHub/dotfiles-secrets/dbkp.toml
 end
