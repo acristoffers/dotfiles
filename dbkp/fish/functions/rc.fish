665632fd15a0a573d8f520a29102d7f8799608be
Álan Crístoffer <acristoffers@startmail.com>
2024-03-16 10:59:28 -0300

    Sat Mar 16 10:59:28 2024

--- dbkp/fish/functions/rc.fish
+++ dbkp/fish/functions/rc.fish
@@ -2,6 +2,6 @@ function rc
   if test (uname -s) = "Darwin"
     darwin-rebuild switch --flake $XDG_CONFIG_HOME/home-manager
   else
-    home-manager switch
+    home-manager switch --flake $XDG_CONFIG_HOME/nix/home-manager
   end
 end
