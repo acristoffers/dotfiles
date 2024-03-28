1e3e1f8fe5c7ae80702bdcb46dc6f8e3537f429e
Álan Crístoffer <acristoffers@startmail.com>
2024-03-28 18:04:58 -0300

    Thu Mar 28 18:04:57 2024

--- dbkp/fish/functions/update-rustup.fish
+++ dbkp/fish/functions/update-rustup.fish
@@ -4,5 +4,7 @@ function update-rustup
     rm -rf $RUSTUP_HOME
     rustup default stable
     rustup component add rust-analyzer
+    cargo install cargo-edit
+    nix-shell -p stdenv cargo rustc openssl pkg-config --command "cargo install cargo-edit"
   end
 end
