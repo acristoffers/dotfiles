08f6dc46730d5ecfa24313757aec6d1ddbfe820e
Álan Crístoffer <acristoffers@startmail.com>
2024-05-18 19:15:36 +0200

    Sat May 18 19:15:36 2024

--- dbkp/nix/home-manager/dotfiles/tmux/tpm-setup.sh
+++ dbkp/nix/home-manager/dotfiles/tmux/tpm-setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-export XDG_DATA_HOME=${XDG_DATA_HOME:=~/.local/share}
+set -euo pipefail
 
-if [[ ! -d "$XDG_DATA_HOME/tmux/plugins/tpm" ]]; then
-    git clone https://github.com/tmux-plugins/tpm "$XDG_DATA_HOME/tmux/plugins/tpm"
-    "$XDG_DATA_HOME/tmux/plugins/tpm/bin/install_plugins"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-~/.local/share}"
+
+if [[ ! -d "${XDG_DATA_HOME}/tmux/plugins/tpm" ]]; then
+    git clone https://github.com/tmux-plugins/tpm "${XDG_DATA_HOME}/tmux/plugins/tpm"
+    "${XDG_DATA_HOME}/tmux/plugins/tpm/bin/install_plugins"
 fi
 
-"$XDG_DATA_HOME/tmux/plugins/tpm/tpm"
+exec "${XDG_DATA_HOME}/tmux/plugins/tpm/tpm"
