948fb6941f3a7d0345b4fca3f16db8e73188489b
Álan Crístoffer <acristoffers@startmail.com>
2024-05-29 20:30:48 +0200

    Wed May 29 20:30:47 2024

--- dbkp/fish/functions/update-rustup.fish
+++ dbkp/fish/functions/update-rustup.fish
@@ -1,7 +1,7 @@
 function update-rustup
   if type -q rustup
     title Updating Rust
-    rm -rf $RUSTUP_HOME
+    rm -rf $RUSTUP_HOME $XDG_DATA_HOME/cargo
     rustup default stable
     rustup component add rust-analyzer
     nix-shell -p stdenv cargo rustc openssl pkg-config --command "cargo install cargo-edit --force"
