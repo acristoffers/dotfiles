2166496718dfa3da2d806e5d447f50b8470ea44a
Álan Crístoffer <acristoffers@startmail.com>
2026-01-10 16:10:22 +0100

    Sat Jan 10 16:10:22 CET 2026

--- dbkp/fish/functions/fin.fish
+++ dbkp/fish/functions/fin.fish
@@ -1,8 +1,14 @@
 function fin
-  set -l date (grep '* Salaire' ~/.org/finances/2025.ledger | awk '{print $1}' | tail -n 1 | string split /)
-  set -l date $date[2]/$date[3]
-  ledger -f ~/.org/finances/2025.ledger bal Bank:Checking
-  ledger -f ~/.org/finances/2025.ledger bal Bank:Saving
+  set -l year (date +%Y)
+  set -l date (grep '* Salaire' ~/.org/finances/$year.ledger | awk '{print $1}' | tail -n 1 | string split /)
+  if test -n "$date"
+    set date $date[2]/$date[3]
+  else
+    set date $year/01
+  end
+  echo date: $date
+  ledger -f ~/.org/finances/$year.ledger bal Bank:Checking
+  ledger -f ~/.org/finances/$year.ledger bal Bank:Saving
   echo '===================='
-  ledger -f ~/.org/finances/2025.ledger bal -b $date Expenses
+  ledger -f ~/.org/finances/$year.ledger bal -b $date Expenses
 end
